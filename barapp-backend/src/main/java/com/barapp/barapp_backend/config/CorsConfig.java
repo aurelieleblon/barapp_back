c1b4773460a1f78b2a01a7802447a3d5a39ea771
aurelieleblon <petiteaurelie2002@yahoo.fr>
2025-07-03 15:40:19 +0200

    auth controller

--- barapp-backend/src/main/java/com/barapp/barapp_backend/config/CorsConfig.java
+++ barapp-backend/src/main/java/com/barapp/barapp_backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5174")  // origine frontend
+                        .allowedOrigins("http://localhost:5173")  // origine frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true);  // <== important pour autoriser credentials
             }
